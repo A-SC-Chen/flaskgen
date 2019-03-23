5fbbb925f4ff69117c1f677a5c118a9094680ccf
Alan Chen <shichuchen0114@gmail.com>
2019-03-23 14:58:05 -0700

    initializing

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': "Article One",
+            'body': 'lorem',
+            'author': 'No Idea',
+            'create_date': 'lorem'
+        },
+        {
+            'id': 2,
+            'title': "Article Two",
+            'body': 'lorem',
+            'author': 'No Idea',
+            'create_date': 'lorem'
+        },
+        {
+            'id': 3,
+            'title': "Article Three",
+            'body': 'lorem',
+            'author': 'No Idea',
+            'create_date': 'lorem'
+        }
+    ]
+    return articles
